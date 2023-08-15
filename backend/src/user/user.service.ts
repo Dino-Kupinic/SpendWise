2eef7b8537a796a4178c80250303143502dd7b18
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-08-15 16:29:20 +0200

    user passwords are stored as sha256 hash in database, tried using bcrypt or argon but it didnt seem to work (because of docker volumes perhaps?)

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,19 +1,37 @@
 import {Injectable} from "@nestjs/common"
 import {PrismaService} from "../prisma/prisma.service"
 import {Prisma, User} from "@prisma/client"
+import * as crypto from "crypto"
+
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {
   }
 
-  async getUsers() {
-    return this.prisma.user.findMany();
+  /**
+   * Retrieves a list of all users.
+   *
+   * @async
+   * @returns {Promise<User[] | undefined>} A Promise that resolves to an array of user objects.
+   */
+  async getUsers(): Promise<User[] | undefined> {
+    return this.prisma.user.findMany()
   }
 
+  /**
+   * Creates a new user.
+   *
+   * @param {Prisma.UserCreateInput} data - The data required to create a new user.
+   * @return {Promise<User>} - A promise that resolves to the created user.
+   */
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    const hash: string = crypto.createHash("sha256").update(data.password).digest("hex")
     return this.prisma.user.create({
-      data,
+      data: {
+        ...data,
+        password: hash,
+      },
     })
   }
 }
